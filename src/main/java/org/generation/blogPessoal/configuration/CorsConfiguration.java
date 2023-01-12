232bdbdfe67b53f13f7652ba8a06d23c9d23b4c1
Matheus Rosas <matheus251170@outlook.com>
2023-01-12 16:30:30 -0300

    cors configuration class

---
+++ src/main/java/org/generation/blogPessoal/configuration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package org.generation.blogPessoal.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://blogpessoalmatheus.netlify.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
